cf7ace5cc2969dd950a722274b8296d03e64629b
JordanO1999 <jordanenrrique091@gmail.com>
2024-09-15 11:58:39 -0500

    Primer commit

---
+++ pila.py
@@ -0,0 +1,26 @@
+pila [23]
+
+pila.appened(23)
+pila.appened(100)
+pila.appened(25)
+
+print("Actualización del contenedor  entes de pop")
+for elem in containerPila:
+    print(item)
+
+auxl=containerPila.pop()
+
+print('Ultimo elemento en mostrarse {auxl}')
+
+
+print("Actualización del contenedor despues de pop")
+for elem in containerPila:
+    print(item)
+
+aux2=containerPila[-1]
+
+print(f'El ultimo elemento: {aux2}')
+
+print("Actualización del contenedor despues de [-1]")
+for elem in containerPila:
+    print(item)
